45d918e9dbcd753aacb923b4cc7b1bf89a76c1cd
sts5915 <114986341+sts5915@users.noreply.github.com>
2023-07-11 15:20:23 +0900

    Add files via upload

---
+++ practice3.java
@@ -0,0 +1,209 @@
+package practice01;
+
+public class practice3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 3일차 라이브코딩 연습
+		
+		// 문제1
+		// System.out.println('자바 수업은 재밌다!'); 문자열은 '이 아니고 "이다
+		System.out.println("자바 수업은 재밌다!");
+		
+		// 문제2
+		System.out.println('자');
+		System.out.println("자");
+		// System.out.println('자바'); 문자열은 "이므로 오류난다
+		System.out.println("자바");
+		
+		
+		// 문제 3
+		System.out.println("이것은 자바 수업 첫 라이브 코딩 예제입니다.");
+		
+		// 문제4
+		System.out.println("자\n바\n는\n쉽\n다");
+		
+		// 문제5
+		System.out.println("오늘의 날씨는"+" 덥다.");
+		
+		// 문제6
+		System.out.println("가을이는 겨울이에게 \"안녕\" 이라고 말했다.");
+		
+		// 문제7
+		System.out.println("가을이는 겨울이에게 \"안녕\"이라고 말했지만 겨울이는 가을이에게 \"대답\"을 하지 않았다");
+		
+		// 문제8
+		System.out.println("요즘 날씨는 매우 덥습니다. 또한, 곧 장마가 시작될 예정이라 우산을 구매했습니다.\n하지만 우산을 구매하자마자 잃어버렸습니다.\n그래서 속상합니다.");
+		
+		// 문제9
+		//int a=500;
+		//double b=1.51;
+		//System.out.println(a);
+		//System.out.println(b);
+		
+		
+		// 문제10
+		//int a = 200;
+		//int b = 300;
+		//double c = 2.14567892;
+		//System.out.println(a);
+		//System.out.println(b);
+		//System.out.println(c);
+		
+		// 문제 11
+		//int num =200;
+		//int result=num*num;
+		//System.out.println(result);
+		
+		// 문제12
+		//int x =10;
+		//int y = 20;
+		//int temp = x;
+		//x=y;
+		//y=temp;
+		//System.out.println("x의 값은 "+x+" 입니다.");
+		//System.out.println("y의 값은 "+y+" 입니다.");
+		
+		// 문제 13
+		System.out.println("가을이는 겨울이에게\"안녕\"이라고 말했다.");
+		
+		// 문제 14
+		//int x = 50;
+		//int y = 40;
+		//int temp=x;
+		//x=y;
+		//y=temp;
+		//System.out.println("x의 값은 "+x+" 입니다.");
+		//System.out.println("y의 값은 "+y+" 입니다.");
+		
+		// 문제15
+		// 변수는 변하는 값
+		// 임시변수는 값이 변하는동안 변수값이 손실되지 않도록 하기 위헤서 사용한다
+		// 데이터 타입을 변환하는것을 형 변환
+		// 정수형 데이터 타입 int byte short long
+		// 정수 200 과 실수 200.0의 bit값은 다르다
+		// 자동으로 변환되는것이 자동 형 변환
+		// 자동으로 변환되는 특징은 좁은 데이터 타입에서 넓은 데이터 타입으로 변한다
+		// char이 숫자 데이터 타입으로 자동 형 변환되는 이유는 내부적인 특성때문이다.
+		
+		// 문제16
+		//int a=10;
+		//double b=2.0;
+		//double result=a/b;
+		//System.out.println(result);
+		
+		// 문제 17
+		//int a=20;
+		//double b=2.0;
+		//double result=a/b;
+		//System.out.println("a인"+a+"과 b의 "+b+"을 나누기하면 몫은 "+result+"이 된다.");
+		
+		// 문제 18
+		// int a =3;
+		// float b = 20.5F;
+		// double c=a+b;
+		//         int float
+		//        double double
+		//         3.0 + 20.5
+		//  =23.5
+		
+		// 문제19
+		// int a=30;
+		// double b=40.7;
+		// double c=a + b;
+		//         int  double
+		//        double double
+		//         30.0 +40.7
+		//       = 70.7
+		
+		// 문제 20
+		//int a =10;
+		//int b = 30;
+		//double c = 40.5;
+		//double result = a + b + c;
+		//System.out.println(result);
+		
+		// 문제 21
+		// L을 안붙여도 long타입으로 인식하는 이유는 기본인 int범위 내에 포함되어 있으면 인식이 되기 때문이다
+		
+		// 문제 22
+		//int width=10;
+		//int height=40;
+		//int area=width*height;
+		//System.out.println(area);
+		
+		// 문제23
+		//int i=3;
+		//i ++;
+		//System.out.println(i);
+		//++i;
+		//System.out.println(i);
+		//System.out.println(++i);
+		//System.out.println(i++);
+		//System.out.println(i);
+		
+		// 문제24
+		//int x=20;
+		//double y=3.5;
+		//int z=3;
+		//double w=2.5;
+		//double convertedx=(double)x;
+		//int convertedy=(int)y;
+		//double convertedz=(double)z;
+		//int convertedw=(int)w;
+		//double sum=convertedx+convertedy;
+		//double differece=convertedx-convertedy;
+		//double product=convertedz*convertedw;
+		//double division=convertedz/convertedw;
+		//System.out.println("합: "+sum);
+		//System.out.println("차: "+differece);
+		//System.out.println("곱: "+product);
+		//System.out.println("나누기: "+division);
+		
+		// 문제25
+		//double mx =10.5;
+		//double ax = 8.4;
+		//double my=12.2;
+		//double ay=6.3;
+		//double fx=mx*ax;
+		//double fy=my*ay;
+		//System.out.println("x포탄의 힘은 "+fx+", y포탄의 힘은 "+fy+"이다.");
+		//System.out.println("x포탄이 y포탄보다"+fx/fy+"배 더 세다.");
+		
+		// 문제 26
+		//String name="김철수";
+		//int k=85;
+		//int e=90;
+		//int m=92;
+		//int sum=k+e+m;
+		//int avg=sum/3;
+		//System.out.println("이름: "+name);
+		//System.out.println("국어점수: "+k);
+		//System.out.println("영어점수: "+e);
+		//System.out.println("수학점수: "+m);
+		//System.out.println("총점: "+sum);
+		//System.out.println("평균: "+avg);
+		
+		// 문제27
+		int a=5;
+		int b=10;
+		boolean result1=(a==b);
+		System.out.println(result1);
+		boolean result2=(a>b);
+		System.out.println(result2);
+		boolean result3=(a<=b);
+		System.out.println(result3);
+		boolean flag1=true;
+		boolean flag2=false;
+		boolean result4=(flag1!=flag2);
+		System.out.println(result4);
+		
+		// 문제28
+		
+		
+		
+		
+	}
+
+}
