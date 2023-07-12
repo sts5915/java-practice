5c97562ce06338bd05c662d66ad18cb2684b1be1
sts5915 <114986341+sts5915@users.noreply.github.com>
2023-07-12 15:19:30 +0900

    Add files via upload

---
+++ practice6.java
@@ -0,0 +1,149 @@
+package practice01;
+//4일차 (switch case 구문)
+
+import java.util.Scanner;
+public class practice6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("switch(1)");   // 코드분석
+//		switch(1) {						   // switch(1)이면 case뒤 1이 들어간 println구문이 실행된다. 
+//		case 1:							   // 하지만 case(1)만 실행되는 것이 아니라 그뒤 case 구문들도 다 실행된다.
+//			System.out.println("one");     // 여기서 각각에 해당되는것만 출력하려면 break 를 사용한다.
+//		case 2:							   // case 각 구문 아래에 break를 넣는다
+//			System.out.println("two");
+//		case 3:
+//			System.out.println("three");
+//		}	
+//		System.out.println("switch(2)");
+//		switch(2) {
+//		case 1:
+//			System.out.println("one");
+//		case 2:
+//			System.out.println("two");
+//		case 3:
+//			System.out.println("three");
+//		}	
+//		System.out.println("switch(3)");
+//		switch(3) {
+//		case 1:
+//			System.out.println("one");
+//		case 2:
+//			System.out.println("two");
+//		case 3:
+//			System.out.println("three");
+//		}
+//		System.out.println("switch(1)");   
+//		switch(1) {						   
+//		case 1:							   
+//			System.out.println("one");
+//			break;
+//		case 2:							   
+//			System.out.println("two");
+//			break;
+//		case 3:
+//			System.out.println("three");
+//			break;
+//		}	
+//		System.out.println("switch(2)");
+//		switch(2) {
+//		case 1:
+//			System.out.println("one");
+//			break;
+//		case 2:
+//			System.out.println("two");
+//			break;
+//		case 3:
+//			System.out.println("three");
+//			break;
+//		}	
+////		System.out.println("switch(3)");    //switch문과 if문은 서로 대체 가능한 관계이다. 
+//		switch(3) {
+//		case 1:
+//			System.out.println("one");
+//			break;
+//		case 2:
+//			System.out.println("two");
+//			break;
+//		case 3:
+//			System.out.println("three");
+//			break;
+//		}
+		
+		// 퀴즈
+		// 여름 가을
+		// 여름
+		
+		// 문제1
+		/*
+		 * Scanner a = new Scanner(System.in); System.out.println("수를 입력하세요."); int num
+		 * = a.nextInt();
+		 * 
+		 * if(num>0) { System.out.println("양수입니다."); } else if(num<0) {
+		 * System.out.println("음수입니다."); } else { System.out.println("0입니다."); }
+		 */
+		
+		// 문제2
+//		Scanner a = new Scanner(System.in);
+//		System.out.println("입력");
+//		int num = a.nextInt();
+//		System.out.println("switch(num)");
+//		switch(num) {
+//		case 1:
+//			System.out.println("일요일");
+//			break;
+//		case 2:
+//			System.out.println("월요일");
+//			break;
+//		case 3:
+//			System.out.println("화요일");
+//			break;
+//		case 4:
+//			System.out.println("수요일");
+//			break;
+//		case 5:
+//			System.out.println("목요일");
+//			break;
+//		case 6:
+//			System.out.println("금요일");
+//			break;
+//		case 7:
+//			System.out.println("토요일");
+//			break;
+//		
+		// 문제 3
+//		int x = 50;
+//		int y = 60;
+//		if(x<y) {
+//			System.out.println("x는 y보다 작습니다.");
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
