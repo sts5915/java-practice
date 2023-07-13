b599de68765fd708481a11cd631d927dc3a05f0e
sts5915 <114986341+sts5915@users.noreply.github.com>
2023-07-13 14:56:13 +0900

    Add files via upload

---
+++ practice9.java
@@ -0,0 +1,40 @@
+package practice01;
+			// 5일차 조건문 테스트
+public class practice9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+//		System.in.read();는 어떤함수인가?
+//		콘솔창에서 사용자로부터 값을 입력받는 함수
+//		
+//		System.in.read();의 기본 입력값은 아스키코드값이다.
+//		대문자 A의 아스키코드 값은 얼마인가?
+//		65
+//		소문자 a의 아스키코드 값은 얼마인가?
+//		97
+//		대문자 K의 아스키코드 값은 무엇인가?
+//		75	
+//		소문자 z의 아스키코드 값은 무엇인가?
+//		122
+//		아스키코드에서 같은 의미의 문자와 숫자의 코드 값 차이는 얼마인가?
+//		48		
+//		문자'0'의 아스키코드 값은 얼마인가?
+//		48				
+//		문자'2'를 아스키코드 값으로 변환하려면 어떤 과정을 거쳐야 하는가?
+//		2+48=50						
+//		BufferedReader은 무엇을 이용하여 읽기를 하는 함수인가?
+//		버퍼를 사용하여 읽기를 하는 함수								
+//		scanner sc = new scanner(system.in);에서 잘못된것을 수정하시오
+//		Scanner sc = new Scanner(System.in); 대문자 구별해서 써야하나								
+//		Scanner을 이용해서 정수 중 int를 입력받고 싶으면 어떤 메소드를 사용하면 되는가?
+//		int 변수이름 = Scanner변수이름.nextInt();										
+//	    Scanner을 이용해서 실수 중 float을 입력받고 싶으면 어떤 메소드를 사용하면 되는가?
+//	    float 변수이름 = Scanner변수이름.nextFloat();
+//		Scanner을 이용해서 문자열으 입력받고 싶으면 어떤 메소드를 사용하면 되는가?
+//		String 변수이름 = Scanner변수이름.next();
+		
+	}
+
+}
