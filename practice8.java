94ad8b92b726faad7c5da40eb79709e4b5222f1f
sts5915 <114986341+sts5915@users.noreply.github.com>
2023-07-17 12:00:56 +0900

    Add files via upload

--- practice8.java
+++ practice8.java
@@ -14,7 +14,7 @@ public class practice8 {
 //		if((x>=-1000&&x<=1000&&x!=0)&&(y>=-1000&&y<=1000&&y!=0)) {  // if문 하나에 x범위 지정해주고 하위 if문에 y범위 지정해줘도 된다
 //			if(x>0&&y>0) {											// if(x>=-1000&&x<=1000&&x!=0)	
 //				System.out.println("1");							//    if(y>=-1000&&y<=1000&&y!=0)
-//			} else if(x<0&&y>0) {									// 이런식으로도 표현이 가능하다.
+//			} else if(x<0&&y>0) {									// 이런식으로도 표현이 가능하다
 //				System.out.println("2");
 //			} else if(x<0&&y<0) {
 //				System.out.println("3");
