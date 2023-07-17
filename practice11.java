f32940efbce9f625f92795eaa881e2bfe497c3cd
sts5915 <114986341+sts5915@users.noreply.github.com>
2023-07-17 15:20:02 +0900

    Add files via upload

---
+++ practice11.java
@@ -0,0 +1,57 @@
+package practice01;
+		// 6일차 (반복문 문제)
+import java.util.Scanner;
+public class practice11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		// 문제 1
+//		for(int i = 10; i>=1; i--) {
+//			System.out.println(i);
+//		}
+//		
+//		// 문제 2
+//		for(int i=1; i<10; i++) {
+//			System.out.println("7 X "+ i + " = "+ (7*i));
+//			}
+		
+		// 문제 3
+//		for(int i = 1; i<=100; i++) {
+//			if(i%3==0) {
+//				System.out.println(i);
+//			}
+//		}
+//		
+		// 문제 4
+		
+//		int sum=0;	
+//		for(int i=1; i<=100; i++) {
+//			sum+=i;
+//		}
+//		System.out.println(sum);        // 바깥에 써야 최종 합만 출력된다
+		
+		
+		// 문제 5
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("n을 입력하세요: ");
+//		int n = sc.nextInt();
+//		if(n<=100000) {
+//			for(int i=1; i<=n; i++) {
+//				System.out.println(i);
+//			}
+//		}
+		
+		// 문제 6
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("n을 입력하세요(1<=n<=100): ");
+//		int n = sc.nextInt();
+//		for(int i = 1; i<=n; i++) {
+//			for(int j=0; j<i; j++) {
+//				System.out.print("*");
+//			}
+//		System.out.println();
+//		}
+	}
+
+}
