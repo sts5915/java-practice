c193139303e6e9419dcdb01249a2c2d9af81c126
sts5915 <114986341+sts5915@users.noreply.github.com>
2023-07-19 15:15:53 +0900

    Add files via upload

---
+++ practice15.java
@@ -0,0 +1,38 @@
+package practice01;
+		// 9일차
+
+import java.util.Scanner;
+public class practice15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 코딩 테스트 문제 
+		// 문제 1
+		Scanner sc = new Scanner(System.in);
+		System.out.print("n을 입력하세요(1<=n<=10000) : ");
+		int n = sc.nextInt();
+		int sum = 0;
+		for(int i=1; i<=n; i++) {
+			sum += i;
+		}
+		System.out.println("총합 : " +sum);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
